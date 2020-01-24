3e80acdb5efd8d2b8feb3ef33faa5029df785ae8
James Lovejoy <james.lovejoy2@gmail.com>
2020-01-24 15:02:18 -0600

    completed code for view all managers query

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ function mainMenu() {
     //This code runs a list type inquirer for the choice of what option to choose at the main menu.
     inquirer
         .prompt([a]).then(operation => {
-            console.log(operation);
+
             switch (operation.menuChoice) {
                 case commandMenuChoices[0]:
                     return viewAllEmp();
@@ -51,10 +51,11 @@ function mainMenu() {
                     return viewAllManager();
 
                 case commandMenuChoices[5]:
+                //This is the case for adding an employee
 
                     const query1 = "SELECT role.title FROM role"
                     const compRolesArrayQuery = new SQLquery(query1);
-                    
+
                     compRolesArrayQuery.getQueryNoRepeats(addEmp);
                     // return compRoles1.getQueryNoRepeats(addEmp);
                     // return addEmp();
@@ -104,7 +105,7 @@ function viewAllEmpDep() {
 
     const departmentNamePrompt = new InquirerFunctions(inquirerTypes[0], 'department_Name', questions.viewAllEmpByDep);
     inquirer.prompt(departmentNamePrompt.ask()).then(userResp => {
-        const query = query_code.view_All_Query + " AND department.name = ? "
+        const query = query_code.view_All_Query + " AND department.name = ? ;"
         console.log(query);
         console.log(userResp.department_Name);
         const empByDepTable = new SQLquery(query, userResp.department_Name);
@@ -120,7 +121,10 @@ function viewAllEmpManager() {
 function viewAllEmpRole() {
     const rolePrompt = new InquirerFunctions(inquirerTypes[0], 'role_Title', questions.viewAllEmpByRole);
     inquirer.prompt(rolePrompt.ask()).then(userResp => {
-        const query = "SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name FROM employee INNER JOIN role on role.id = employee.role_id AND role.title = ? INNER JOIN department on department.id = role.department_id";
+        const query = `SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name
+                        FROM employee 
+                        INNER JOIN role on role.id = employee.role_id AND role.title = ?
+                        INNER JOIN department on department.id = role.department_id;`;
 
         const empByRoleTable = new SQLquery(query, userResp.role_Title);
 
@@ -130,12 +134,24 @@ function viewAllEmpRole() {
 
 function viewAllManager() {
 
+    //This query selects all the given columns from employee and join
+    const query = `SELECT employee.id, employee.first_name, employee.last_name, department.name
+                    FROM employee
+                    INNER JOIN role on role.id = employee.role_id
+                    INNER JOIN department on department.id = role.department_id
+                    WHERE employee.id IN ( SELECT employee.manager_id FROM employee );`;
+    
+    const managerTable = new SQLquery(query);
+
+    managerTable.generalTableQuery(mainMenu);
 }
 
+//Function that receives the input from the user and then adds the proper employee values into the employee table in my database
 function addEmp(compRoles) {
 
+    //This query selects all the given columns from employee and where employee.id is present in the selected table of employee_ids present in employee_manager column.
     const query = "SELECT id, first_name, last_name FROM employee WHERE employee.id IN ( SELECT employee.manager_id FROM employee )";
-       
+                    
     connection.query(query, function (err, res) {
         if (err) {
             throw err
@@ -145,16 +161,18 @@ function addEmp(compRoles) {
 
         let managerNamesArr = [];
         let managerObjArr = [];
-                for (let i=0; i < res.length; i++) {
+        for (let i = 0; i < res.length; i++) {
 
-                    let name = res[i].first_name + " " + res[i].last_name;
-                        let managersobj = { ID: res[i].id,
-                            firstName: res[i].first_name,
-                            lastName: res[i].last_name}
-                        
-                        managerObjArr.push(managersobj);
-                        managerNamesArr.push(name);
-                }
+            let name = res[i].first_name + " " + res[i].last_name;
+            let managersobj = {
+                ID: res[i].id,
+                firstName: res[i].first_name,
+                lastName: res[i].last_name
+            }
+
+            managerObjArr.push(managersobj);
+            managerNamesArr.push(name);
+        }
 
         const first_name = new InquirerFunctions(inquirerTypes[0], 'first_name', questions.addEmployee1);
         const last_name = new InquirerFunctions(inquirerTypes[0], 'last_name', questions.addEmployee2);
@@ -164,59 +182,48 @@ function addEmp(compRoles) {
         Promise.all([first_name.ask(), last_name.ask(), emp_role.ask(), emp_manager.ask()]).then(prompts => {
             inquirer.prompt(prompts).then(emp_info => {
 
-                // "SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name
-                //  FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id"
-                // console.log(emp_info);
-                
-
                 const queryRoleIdFromTitle = "SELECT role.id FROM role WHERE role.title = ? "
-                
-                connection.query(queryRoleIdFromTitle, emp_info.employee_role ,function(err, res){
-                    if(err) {
+
+                connection.query(queryRoleIdFromTitle, emp_info.employee_role, function (err, res) {
+                    if (err) {
                         throw err;
                     }
                     const empRoleId = res[0].id;
                     const empFirstName = emp_info.first_name;
                     const empLastName = emp_info.last_name;
                     const empManagerName = emp_info.employee_manager.split(" ");
-                    const empManagerFirstName = empManagerName[0] ;
+                    const empManagerFirstName = empManagerName[0];
                     const empManagerLastName = empManagerName[1];
-                    
+
                     let empManagerID = 0;
-                   
+
                     for (let manager of managerObjArr) {
                         if (manager.firstName == empManagerFirstName && manager.lastName === empManagerLastName) {
                             console.log("Found Manager ID");
                             empManagerID = manager.ID;
                         }
                     }
-            
+
                     const queryInsertEmpInfo = "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)"
-                    
-                    connection.query(queryInsertEmpInfo, [empFirstName, empLastName, empRoleId, empManagerID], function(err,res){
+
+                    connection.query(queryInsertEmpInfo, [empFirstName, empLastName, empRoleId, empManagerID], function (err, res) {
                         if (err) {
                             throw err
                         }
                         console.log("Employee Added");
-                        console.log(res);
                         mainMenu();
                     })
                 })
-                })
-
-                
-                // const value = 
-                // const insertQuery = new SQLquery(query, )
-
             })
         })
+    })
 }
-// Promise.all([a,b,c]).then(prompts => {
-//     inquirer
-//         .prompt(prompts).then(resp => {
-//             console.log(resp);
-//         })
-// })
+
+function removeEmp() {
+
+
+}
+
 
 
 
